fe15b352a1df428aabe7a3515dadd18f95c7f71d
gpperez <gpperez@gmail.com>
2022-05-03 21:50:37 -0300

    [ODOO-3587][ADD][om_account_followup_blocked_fix] - Emails sent to the client will not include blocked documents

---
+++ om_account_followup_blocked_fix/__manifest__.py
@@ -0,0 +1,23 @@
+# pylint: disable=missing-module-docstring,pointless-statement
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Customer Follow Up Management (Fix Blocked mail)",
+    "summary": """
+        Summary of the module's purpose
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ['gpperez'],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "version": "13.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": ['om_account_followup'],
+
+}
