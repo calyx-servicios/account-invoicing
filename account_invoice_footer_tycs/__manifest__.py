d8716ebd17de6365eee11d5b78347d765c23fc29
deykerag <deykergil28@gmail.com>
2022-05-16 15:16:16 -0400

    [ODOO-2821][ADD][account_invoice_footer_tycs]     - Add Module to customize the Dentons report footer

---
+++ account_invoice_footer_tycs/__manifest__.py
@@ -0,0 +1,18 @@
+# pylint: disable=missing-module-docstring,pointless-statement
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+
+    "name": "Invoice Footer TyC",
+    "summary": """
+        This module adds TyC to the footer, between qr and CAE info 
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["ParadisoCristian", "DeykerGil"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "version": "13.0.1.0.0",
+    "depends": ["l10n_ar_edi"],
+    'data': [
+        'views/report_invoice.xml',
+    ],
+}
