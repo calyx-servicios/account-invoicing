a230def7955a85cb82c48a0cae305fd1529103a3
Marco Oegg <marco.oegg@gmail.com>
2022-08-18 17:19:21 -0300

    [UPD][cx_api_invoice_payments] - add changelog - upg module version

--- cx_api_invoice_payments/__manifest__.py
+++ cx_api_invoice_payments/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Accounting",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
