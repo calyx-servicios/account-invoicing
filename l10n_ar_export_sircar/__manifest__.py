0a62f9df764c2393bb3df25bd65ca1073faad389
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-04-14 18:16:59 -0300

    [ODOO-6401][UPD][l10n_ar_export_sircar] - Update to v15

---
+++ l10n_ar_export_sircar/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Export SIRCAR",
+    "summary": """
+        This module generates the SIRCAR file for 
+        withholdings and perceptions.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["PerezGabriela"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Account",
+    "version": "15.0.1.0.0",
+    'installable': True,
+    'application': False,
+    "depends": [
+        "l10n_ar_ux",
+        "account_payment_group",
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/account_view.xml',
+    ],
+}
