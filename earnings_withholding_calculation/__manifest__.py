eb954337107b7b3d5d80ca6e13cddf774775f5b3
cristian <crisdparadiso@gmail.com>
2023-06-16 17:11:24 -0300

    [ODOO-6750][IMP][earnings_withholding_calculation]

---
+++ earnings_withholding_calculation/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Earnings Withholding Calculation",
+    "summary": """
+        This module performs the calculation of 
+        earnings withholdings.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["PerezGabriela"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Account",
+    "version": "15.0.1.0.0",
+    "installable": True,
+    "application": False,
+    "depends": [
+        "l10n_ar_account_withholding"
+    ],
+    "data": [
+        "data/account_journal.xml",
+        "data/account_tax.xml",
+        "views/account_payment_group_view.xml"
+    ],
+}
