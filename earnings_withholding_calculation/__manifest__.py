8222546764b1fe4aa6d5eec71b6b12250780c3aa
gabbiiperez <gabbiiperez94@gmail.com>
2023-06-26 13:51:49 -0300

    [ODOO-6782][IMP][earnings_withholding_calculation][l10n_ar_export_sicore] - IMP new modules.

--- earnings_withholding_calculation/__manifest__.py
+++ earnings_withholding_calculation/__manifest__.py
@@ -1,9 +1,9 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    "name": "Export Sicore",
+    "name": "Earnings Withholding Calculation",
     "summary": """
-        This module generates the sicore file for 
-        withholdings and perceptions.
+        This module performs the calculation of 
+        earnings withholdings.
     """,
     "author": "Calyx Servicios S.A.",
     "maintainers": ["PerezGabriela"],
@@ -14,12 +14,11 @@
     "installable": True,
     "application": False,
     "depends": [
-        "l10n_ar_ux",
-        "account_payment_group",
+        "l10n_ar_account_withholding"
     ],
     "data": [
-        "security/ir.model.access.csv",
-        "views/account_view.xml",
-        "views/account_tax_view.xml",
+        "data/account_journal.xml",
+        "data/account_tax.xml",
+        "views/account_payment_group_view.xml"
     ],
 }
