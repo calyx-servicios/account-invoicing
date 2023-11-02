dc59415a2daf2d0bd6489f6d79137f06cde5b98c
Leandro Ferrari <111808524+Leandro090685@users.noreply.github.com>
2023-11-02 10:31:45 -0300

    Add files via upload

--- l10n_ar_export_sircar/__manifest__.py
+++ l10n_ar_export_sircar/__manifest__.py
@@ -6,11 +6,11 @@
         withholdings and perceptions.
     """,
     "author": "Calyx Servicios S.A.",
-    "maintainers": ["PerezGabriela"],
+    "maintainers": ["PerezGabriela", "leandro090685"],
     "website": "http://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Account",
-    "version": "13.0.4.0.0",
+    "version": "13.0.5.0.0",
     'installable': True,
     'application': False,
     "depends": [
@@ -22,3 +22,4 @@
         'views/account_view.xml',
     ],
 }
+
